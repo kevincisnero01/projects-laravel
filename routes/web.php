acbb1a994704498af3641ad807cea69751db2727
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-07-23 23:14:36 -0430

    Routes & views mostrar HTML

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,41 @@
 <?php
 
-Route::get('/', function(){
-    echo "<a href='".route('contacto')."'>Contacto 1</a><br>";
-    echo "<a href='".route('contacto')."'>Contacto 2</a><br>";
-    echo "<a href='".route('contacto')."'>Contacto 3</a><br>";
-    echo "<a href='".route('contacto')."'>Contacto 4</a><br>";
-    echo "<a href='".route('contacto')."'>Contacto 5</a><br>";
+Route::view('/', 'home')->name('home');
+
+Route::view('/about', 'about')->name('about');
+
+Route::view('/portfolio', 'portfolio')->name('portfolio');
+
+Route::view('/contact', 'contact')->name('contact');
+
+/*
+//Retornar vistas #1
+Route::get('/', function(){ 
+    return view('home'); 
 });
 
-//Route Name: se hace referencia al nombre de la ruta y no a la URL
-Route::get('contactame/{name?}', function($name = "invitado"){
-    return "hola $name, Bienvenido a contacto";
-})->name('contacto');
+//Pasar vistas #2 forma simplificada 
+Route::view('/', 'home'); // Ej: para paginas como politicas de privacidad, terminos y condiciones, etc
+
+//Pasar datos #4 con la funcion compact() que crea un array con los datos recibidos como parametros
+Route::get('/', function(){ 
+    $nombre = "yheisson"; 
+    return view('home', compact('nombre')); //Ej: ['nombre' => $nombre]
+});
+
+//Pasar datos #3 con la funcion view() pasandole un array como parametros
+Route::get('/', function(){ 
+    return view('home',['nombre' =>'genesis']); 
+});
+
+//Pasar datos #2 con la funcion with() pasandole un array como parametro
+Route::get('/', function(){ 
+    return view('home')->with(['nombre' =>'vaneska']); 
+});
+
+//Pasar datos #1 con la funcion with() 
+Route::get('/', function(){ 
+    return view('home')->with('nombre','kevin'); 
+});
+
+*/
