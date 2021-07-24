fe2b1ea62f295d65eba8df0fb81690a79b0cb581
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-07-24 14:52:10 -0430

    Blade Introduccion

--- routes/web.php
+++ routes/web.php
@@ -7,35 +7,3 @@ Route::view('/about', 'about')->name('about');
 Route::view('/portfolio', 'portfolio')->name('portfolio');
 
 Route::view('/contact', 'contact')->name('contact');
-
-/*
-//Retornar vistas #1
-Route::get('/', function(){ 
-    return view('home'); 
-});
-
-//Pasar vistas #2 forma simplificada 
-Route::view('/', 'home'); // Ej: para paginas como politicas de privacidad, terminos y condiciones, etc
-
-//Pasar datos #4 con la funcion compact() que crea un array con los datos recibidos como parametros
-Route::get('/', function(){ 
-    $nombre = "yheisson"; 
-    return view('home', compact('nombre')); //Ej: ['nombre' => $nombre]
-});
-
-//Pasar datos #3 con la funcion view() pasandole un array como parametros
-Route::get('/', function(){ 
-    return view('home',['nombre' =>'genesis']); 
-});
-
-//Pasar datos #2 con la funcion with() pasandole un array como parametro
-Route::get('/', function(){ 
-    return view('home')->with(['nombre' =>'vaneska']); 
-});
-
-//Pasar datos #1 con la funcion with() 
-Route::get('/', function(){ 
-    return view('home')->with('nombre','kevin'); 
-});
-
-*/
