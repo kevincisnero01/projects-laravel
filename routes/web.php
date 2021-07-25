6808dfc30d60892c82a72a403720065f3fbed52c
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-07-25 16:15:34 -0430

    Controladores Introduccion

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
- $portfolio = [
-	['title' => 'Proyecto # 1'],
-	['title' => 'Proyecto # 2'],
-	['title' => 'Proyecto # 3'],
-	['title' => 'Proyecto # 4'],
-];
+use App\Http\Controllers\PortfolioController;
 
 Route::view('/', 'home')->name('home');
 
 Route::view('/about', 'about')->name('about');
 
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', PortfolioController::class)->name('portfolio');
 
 Route::view('/contact', 'contact')->name('contact');
