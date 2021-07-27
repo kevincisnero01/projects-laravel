6049ae20c460d6442d7a590e176f46dc343aabdd
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-07-26 21:33:55 -0430

    Link de navegacion

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,5 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 
 Route::get('/portfolio', [PortfolioController::class,'index'])->name('portfolio');
-//Route::resource('portfolios', PortfolioController::class)->only('index');
 
 Route::view('/contact', 'contact')->name('contact');
