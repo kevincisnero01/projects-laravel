2320c2b762de843b8946840cffdc5fa68796dd77
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-07-26 22:32:00 -0430

    Enviar Formularios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\ContactController;
 
 Route::view('/', 'home')->name('home');
 
@@ -9,3 +10,5 @@ Route::view('/about', 'about')->name('about');
 Route::get('/portfolio', [PortfolioController::class,'index'])->name('portfolio');
 
 Route::view('/contact', 'contact')->name('contact');
+
+Route::post('/contact', [ContactController::class,'store'])->name('contact.store');
