69ad91988af62e0eee61cbb813083d42dae71307
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-09-15 22:45:54 -0430

    Route Resource Proyectos

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,9 @@ Route::view('/', 'home')->name('home');
 
 Route::view('/quienes-somos', 'about')->name('about');
 
-Route::get('/proyectos', [ProjectController::class,'index'])->name('projects.index');
-Route::get('/proyectos/create', [ProjectController::class,'create'])->name('projects.create');
-Route::get('/proyectos/{project}', [ProjectController::class,'show'])->name('projects.show');
-Route::post('/proyectos', [ProjectController::class,'store'])->name('projects.store');
-Route::get('/proyectos/{project}/edit', [ProjectController::class,'edit'])->name('projects.edit');
-Route::put('/proyectos/{project}', [ProjectController::class,'update'])->name('projects.update');
-Route::delete('/proyectos/{project}', [ProjectController::class,'destroy'])->name('projects.destroy');
+Route::resource('proyecto', ProjectController::class)
+->names('projects')
+->parameters(['proyecto' => 'project']);
 
 Route::view('/contacto', 'contact')->name('contact');
 Route::post('/contacto', [ContactController::class,'store'])->name('contact.store');
