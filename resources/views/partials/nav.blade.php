51d3d0ac0f466f39388e76b4f037c1e6704217c1
Kevin Cisnero <kevingabriel18@gmail.com>
2021-09-22 22:51:55 -0400

    Menu de navegacion | Submenu | Mostrar el usuario de la sesion

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,10 @@
-<nav class="navbar navbar-light navbar-expand-sm bg-white shadow-sm">
+<nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm ">
 	<div class="container">
+
 		<a class="navbar-brand" href="{{ route('home') }}">
-		{{ config('app.name') }}
+			{{ config('app.name') }}
 		</a>
+
 		<button class="navbar-toggler" type="button"
 			data-toggle="collapse"
 			data-target="#navbarSupportedContent"
@@ -11,8 +13,9 @@
 			aria-label="{{ __('Toggle navigation') }}"
 		> <span class="navbar-toggler-icon"></span>
 		</button>
+
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="nav nav-pills nav-brand">
+			<ul class="nav nav-pills nav-brand mr-auto">
 				<li class="nav-item" >
 					<a class="nav-link {{ setActive('home') }}" href="{{ route('home') }}" > @lang('Home') </a>
 				</li>
@@ -25,22 +28,39 @@
 				<li class="nav-item" >
 					<a class="nav-link {{ setActive('contact') }} "href="{{ route('contact') }}"> @lang('Contact') </a>
 				</li>
-				@auth
-					@if(Auth::user())
-					<li class="nav-item">
-						<a class="nav-link" href="{{ route('register') }}">Usuarios</a>
-					</li>
-					@endif
-					<li class="nav-item">
-						<a class="nav-link" class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Cerrar sesion	</a>
-					</li>
-				@else
+			</ul>
+
+			<ul class="nav navbar-nav">
+				@guest
 					<li class="nav-item">
 						<a class="nav-link" href="{{ route('login') }}">Login</a>
 					</li>
+				@endguest
+				@auth
+				<li class="nav-item dropdown ">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img class="rounded-circle mr-2" src="/img/profile.png" width="30" height="30" alt="profile">
+						{{ Auth::user()->name}}
+					</a>
+
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+						<a class="dropdown-item" href="#">Perfil</a>
+						
+						<a class="dropdown-item" href="#">Usuarios</a>
+
+						<a class="dropdown-item" href="#">Configuraciones</a>
+
+						<div class="dropdown-divider"></div>
+
+						<a class="dropdown-item" class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Cerrar sesion	</a>
+
+					</div>	
+				</li>
 				@endauth
 			</ul>
 		</div><!--#navbarSupportedContent-->
+
 	</div><!--container-->
 </nav>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
